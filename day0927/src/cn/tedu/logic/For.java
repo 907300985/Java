82786a86f29fd5c75d2ca43880898c142144faa5
NANDI GUO <44260801+907300985@users.noreply.github.com>
2021-09-27 14:51:14 +0800

    Create For.java

---
+++ day0927/src/cn/tedu/logic/For.java
@@ -0,0 +1,46 @@
+package cn.tedu.logic;
+//测试循环结构
+public class For {
+	public static void main(String[] args) {
+		method(100);
+		method2(10);
+		method3(10);
+		method4();
+		method5();
+	}
+
+	//100次名字	
+	private static void method(int t) {
+		for (int i = 0; i <= t; ++i) {
+			System.out.println(i+" 次Rimuru");
+		}
+	}
+	//1-10
+	private static void method2 (int t) {
+		for (int i = 0; i <= t; ++i) {
+			System.out.println(i);
+		}
+	}
+	//倒数10-1
+	private static void method3 (int t) {
+		for (int i = t; i >= 1; --i) {
+			System.out.println(i);
+		}
+	}
+	//输出8算法
+	private static void method4 () {
+		for (int i = 8; i <= 8888; i=i*10+8) {
+			System.out.print(i+",");
+		}
+	}
+	//求和偶数1-100
+	private static void method5() {
+		int sum = 0;
+		for (int i = 0; i <= 100; i++) {
+			if (i%2 == 0) {
+				sum += i;
+			}
+		}
+		System.out.println("\n"+sum);	
+	}
+}
