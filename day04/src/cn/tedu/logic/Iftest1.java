54ef11e838f7fc6a5e76554b0224d71799be8a20
NANDI GUO <44260801+907300985@users.noreply.github.com>
2021-09-27 13:29:03 +0800

    Update Iftest1.java

--- day04/src/cn/tedu/logic/Iftest1.java
+++ day04/src/cn/tedu/logic/Iftest1.java
@@ -8,32 +8,33 @@ public class Iftest1 {
 		while (true) {
 			System.out.println("请输入你的价格");
 			double price = new Scanner(System.in).nextDouble();
-			dazhe(price);		
+			double x = dazhe(price);
+			System.out.println("原价是"+price+", 打折后为"+x);
 		}
 	}
 
-	public static void dazhe(double price) {
+	public static double dazhe(double price) {
 		double afterprice = 0;
 		
 		if (price >= 5000 ) {
 			afterprice = price * 0.5;
-			dz(price,afterprice);
+			
 		}
 		else if ( price >= 2000) {
 			afterprice = price * 0.8;
 		}
 		else if (price >= 1000) {
 			afterprice = price * 0.9;
-			dz(price,afterprice);
+			
 		}
 		else {
 			afterprice = price;
 			System.out.println("不打折");
 		}
-		//System.out.println("原价是"+price+", 打折后为"+afterprice);
+		return afterprice;
 	}
 
-	private static void dz(double price, double afterprice) {
-		System.out.println("原价是"+price+", 打折后为"+afterprice);
-	}
+//	private static void dz(double price, double afterprice) {
+//		System.out.println("原价是"+price+", 打折后为"+afterprice);
+//	}
 }
