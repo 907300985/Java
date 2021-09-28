84b7a54ff0d3544fefbe839984d383b53ff5622c
NANDI GUO <44260801+907300985@users.noreply.github.com>
2021-09-28 17:59:03 +0800

    normal

--- day0928/src/cn/tedu/idea/HelloIdea.java
+++ day0928/src/cn/tedu/idea/HelloIdea.java
@@ -1,7 +1,12 @@
 package cn.tedu.idea;
 
+import java.util.Scanner;
+
 public class HelloIdea {
     public static void main(String[] args) {
-        System.out.println("Hello Idea!");
+        System.out.println("输入你的话");
+        String a = new Scanner(System.in).next();
+        System.out.println(a);
+
     }
 }
