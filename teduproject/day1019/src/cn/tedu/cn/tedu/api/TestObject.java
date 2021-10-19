eb445938c6516dcb4808f531b86e79e7ed19adba
NANDI GUO <44260801+907300985@users.noreply.github.com>
2021-10-19 19:20:32 +0800

    null

--- teduproject/day1019/src/cn/tedu/cn/tedu/api/TestObject.java
+++ teduproject/day1019/src/cn/tedu/cn/tedu/api/TestObject.java
@@ -1,5 +1,7 @@
 package cn.tedu.cn.tedu.api;
 
+import java.util.Objects;
+
 /**
  * @Date:2021/10/19 15:01
  * @Author:NANDI_GUO
@@ -25,10 +27,24 @@ public class TestObject {
         System.out.println(s1.hashCode());
         System.out.println(s2.hashCode());
         /*负责打印s2对象的是println方法，这个方法会层层向下一直到Object中的toString方法
-        * Object类中toString默认方法打印的是对象的地址值【类名@十六进制哈希码值】
+        * Object类中toString默认方法打印的是对象的地址值【类名@十六进制哈希码 值】
         * 子类重写了Object中的toString方法，打印的是对象的类型+属性+属性值*/
         //测试toString()
         System.out.println(s2);
+        System.out.println(s2.equals(s3));
+        System.out.println(s2==s3);
+        /*
+        * Object 中的equals的默认实现是==比较
+        * ==比较的是左右两边的值，如果是基本类型，比较的是字面值
+        * 比如1和1，3.4和3，4
+        * 如果是引用类型，比较的是引用类型变量保存的地址值*/
+
+        /**
+         * equals（）与hashcode（）逻辑要保持一致，要重写全部重写
+         * 不重写：hashcode（）的生成根据地址值生成
+         *       equals（）帝曾使用过==比较两个对象的地址值
+         * 重写：hashcode（）的哈希码值根据重写传入的属性值生成
+         *      equals（）比较的是重写后的类型+所有属性与属性值*/
     }
 }
 class Student{
@@ -57,5 +73,21 @@ class Student{
                 ", score=" + score +
                 ", Grade='" + Grade + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age &&
+                Double.compare(student.score, score) == 0 &&
+                Objects.equals(name, student.name) &&
+                Objects.equals(Grade, student.Grade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, score, Grade);
     }
 }
---
+++ teduproject/day1019/src/cn/tedu/cn/tedu/api/TestString.java
@@ -0,0 +1,35 @@
+package cn.tedu.cn.tedu.api;
+
+/**
+ * @Date:2021/10/19 16:33
+ * @Author:NANDI_GUO
+ */
+public class TestString {
+    public static void main(String[] args) {
+        String s1 = new String();
+        System.out.println("查看s1"+s1);
+
+        //创建String对象的方式1
+        /*1.字符串类型底层维护的是char[].存在堆内存中*/
+        char[] values = {'a','b','c'};
+        String s2 = new String(values);
+        String s22 = new String(values);
+        System.out.println("查看"+s2);
+        /*此种创建方式不仅写法简单，效率还高，字符串存在堆内存中的常量池
+        * 效果：如果第一次创建，会正常放入常量池，但第二次创建就不会在常量池里新建了
+        * 使用的是之前创建好的值*/
+        String s3 = "abc";
+        String s33 = "abc";
+        System.out.println("查看"+s3);
+        System.out.println(s2 == s22);//false new了两次，两个对象
+        System.out.println(s3 == s33);//true，都指向常量池中的abc,只有一份
+        System.out.println(s2 == s3);//false，一个堆一个常量池
+
+        System.out.println(s2.equals(s22));//true 比较字符串的具体内容
+        String s4 = "hello";
+        System.out.println(s4.equals(s2));//false
+        /**Object中equals默认使用==比较地址值
+         * 但String重写了这个方法，比较的是两个字符串的具体内容
+         * 不论创建方式，只要字符串的内容一样，equals（）就返回true*/
+    }
+}
