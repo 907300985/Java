1d255a8512f52a99d50faa552e335462198d4533
NANDI GUO <44260801+907300985@users.noreply.github.com>
2021-10-29 22:37:05 +0800

    javafx

---
+++ teduproject/day1026/src/cn/tedu/review/exam.java
@@ -0,0 +1,26 @@
+package cn.tedu.review;
+
+/**
+ * @Date:2021/10/29 18:28
+ * @Author:NANDI_GUO
+ */
+public class exam {
+    static  int  i;
+    public static void main(String[] args){
+        System.out.println(i);
+        String  String="String";
+        int[] arr;
+        System.out.println(( 5>3 ) ? !false:false);
+        String str = "";
+    }
+    public static void eat(){
+        System.out.println("asdas");
+    }
+
+}
+
+class  MyClass extends exam {
+    public static void eat(){
+        System.out.println("asdas");
+    }
+}
