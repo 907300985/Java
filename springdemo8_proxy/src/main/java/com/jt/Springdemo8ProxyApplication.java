94d8db57fe046d830e2784fbc48b3a266b0c360b
RimuruG <907300985@qq.com>
2021-12-04 17:44:22 +0800

    update

---
+++ springdemo8_proxy/src/main/java/com/jt/Springdemo8ProxyApplication.java
@@ -0,0 +1,13 @@
+package com.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springdemo8ProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springdemo8ProxyApplication.class, args);
+    }
+
+}
--- vueworkspace/vue/jt01/src/App.vue
+++ vueworkspace/vue/jt01/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-
-    <img src="./assets/l.png" style="border-radius: 10px; ">
+    <img src="./assets/l.png" style="border-radius: 10px; " />
 
     <Person></Person>
 
@@ -11,27 +10,27 @@
       <router-link to="/t1">t1模式</router-link>
       <router-link to="/t2">t2模式</router-link>
     </p>
-    <router-view/>
-
+    <router-view />
   </div>
 </template>
 
 <script>
-  //导入指定的自定义组件
-  import Person from "./components/Person.vue"
-  import Student from "./components/Student.vue"
-  export default {
-    name: 'App',
-    components:{//添加组件
-      Person,
-      Student
-    }
+//导入指定的自定义组件
+import Person from "./components/Person.vue";
+import Student from "./components/Student.vue";
+export default {
+  name: "App",
+  components: {
+    //添加组件
+    Person,
+    Student
   }
-  </script>
+};
+</script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
